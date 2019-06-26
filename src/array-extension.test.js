29fc601c1a83d04f582a17faec160f22d28c0f04
Brayan Ciudad <linzeur@gmail.com>
2019-06-25 22:31:13 -0500

    Add test

--- src/array-extension.test.js
+++ src/array-extension.test.js
@@ -13,10 +13,15 @@ describe("it should return an element random from array", () => {
 });
 
 describe("it should return the first numbers of elements from array", () => {
-  // it("should return the first 4 numbers", () => {
-  //   let arrTemp = [4, 5, 2, 6, 21, 12, 6];
-  //   expect(take(arrTemp, 4)).toBe([4, 5, 2, 6]);
-  // });
+  it("should return the first 4 numbers", () => {
+    let arrTemp = [4, 5, 2, 6, 21, 12, 6];
+    let outcome = take(arrTemp, 4);
+    expect(outcome.length).toBe(4);
+    expect(outcome[0]).toBe(4);
+    expect(outcome[1]).toBe(5);
+    expect(outcome[2]).toBe(2);
+    expect(outcome[3]).toBe(6);
+  });
 
   it("should return undefined when the number passed is negative", () => {
     let arrTemp = [4, 6];
